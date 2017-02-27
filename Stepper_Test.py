0935b8294874228c47b78300712ec9d70fda86cd
Unknown <philipp.jonas@gmx.at>
2017-02-27 11:42:49 +0100

    test step count to 100

--- Stepper_Test.py
+++ Stepper_Test.py
@@ -5,7 +5,7 @@ from stepper_motor_BIPol import StepperMotor
 def main():
     motor_x = StepperMotor([17, 18, 27, 22], 20, 21)
     print("Counter is: "+str(motor_x.counter_A))
-    for i in range(0, 10):
+    for i in range(0, 1000):
         motor_x.step(1)
     print("Counter is: "+str(motor_x.counter_A))
 
