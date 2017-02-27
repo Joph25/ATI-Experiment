2530034644b150cf766324fd599a353db49b1dcb
Unknown <philipp.jonas@gmx.at>
2017-02-27 11:45:46 +0100

    shorter wait time

--- Stepper_Test.py
+++ Stepper_Test.py
@@ -5,10 +5,10 @@ from stepper_motor_BIPol import StepperMotor
 def main():
     motor_x = StepperMotor([17, 18, 27, 22], 20, 21)
     print("Counter is: "+str(motor_x.counter_A))
-    for i in range(0, 100):
+    for i in range(0, 300):
         motor_x.step(1)
     print("Counter is: "+str(motor_x.counter_A))
-    for i in range(0, 100):
+    for i in range(0, 300):
          motor_x.step(0)
     print("Counter is: "+str(motor_x.counter_A))
 
