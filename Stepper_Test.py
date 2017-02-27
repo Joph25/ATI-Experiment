3d2e1e8c6f26fd7170030419865b9be3e021f5d7
Unknown <philipp.jonas@gmx.at>
2017-02-27 11:44:25 +0100

    testing reverse step

--- Stepper_Test.py
+++ Stepper_Test.py
@@ -5,8 +5,11 @@ from stepper_motor_BIPol import StepperMotor
 def main():
     motor_x = StepperMotor([17, 18, 27, 22], 20, 21)
     print("Counter is: "+str(motor_x.counter_A))
-    for i in range(0, 1000):
+    for i in range(0, 100):
         motor_x.step(1)
+    print("Counter is: "+str(motor_x.counter_A))
+    for i in range(0, 100):
+         motor_x.step(0)
     print("Counter is: "+str(motor_x.counter_A))
 
 
