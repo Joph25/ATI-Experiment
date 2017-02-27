e167438872343afdd21d8cf8749269d9d9bbc79f
Unknown <philipp.jonas@gmx.at>
2017-02-27 11:39:48 +0100

    print counter

--- Stepper_Test.py
+++ Stepper_Test.py
@@ -4,9 +4,10 @@ from stepper_motor_BIPol import StepperMotor
 # ---------------------------------------------------------------------------------------------------------
 def main():
     motor_x = StepperMotor([17, 18, 27, 22], 20, 21)
+    print("Counter is: "+str(motor_x.counter_A))
     for i in range(0, 10):
         motor_x.step(1)
-
+    print("Counter is: "+str(motor_x.counter_A))
 
 
 # ---------------------------------------------------------------------------------------------------------
