f53aae8ae722456cb40f0d5dd3c97501ac974934
Joph25 <philipp.jonas@gmx.at>
2017-03-06 18:29:23 +0100

    XML test 4

--- Test/test_xml.py
+++ Test/test_xml.py
@@ -1,5 +1,24 @@
 import xmltodict
+from stepper_motor_BIPol import StepperMotor
 
-with open('config.xml') as fd:
-    doc = xmltodict.parse(fd.read())
-print(doc)
+
+# ---------------------------------------------------------------------------------------------------------
+def main():
+    #read config parameters from config.xml
+    with open('config.xml') as fd:
+        conf = xmltodict.parse(fd.read())
+    print(conf)
+    port_A = conf['config_stepper_motor']['motor1']['port_A']
+    print(port_A)
+    #motor_x = StepperMotor([17, 18, 27, 22], 20, 21)
+    #print("Counter is: "+str(motor_x.counter_A))
+    #for i in range(0, 300):
+    #    motor_x.step(1)
+    #print("Counter is: "+str(motor_x.counter_A))
+    #for i in range(0, 300):
+    #     motor_x.step(0)
+    #print("Counter is: "+str(motor_x.counter_A))
+
+
+# ---------------------------------------------------------------------------------------------------------
+__name__ == '__main__' and main()
